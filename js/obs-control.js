ae7110e6c2a93f757f8d70b498118c2f6b9eab86
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2022-05-21 17:53:38 +0900

    曲専用シーンの2回目が適用されないため修正、開始・終了シーンも曲専用にできるようにした。

--- js/obs-control.js
+++ js/obs-control.js
@@ -25,11 +25,18 @@ let obs;
 let obs_timeout_id;
 let obs_full_combo = true;
 let song_scene_list = false;
-let song_scene = false;
 let song_scene_next_time = false;
 let song_scene_next_name = "";
+let song_scene_time_index = 0;
+let song_scene_time_list = false;
 let song_scene_start_change = true;
 let song_scene_end_change = true;
+let song_scene_game_name = false;
+let song_scene_start_name = false;
+let song_scene_start_duration = false;
+let song_scene_menu_name = false;
+let song_scene_end_name = false;
+let song_scene_end_duration = false;
 const obs_not_rec_audio = new Audio(obs_not_rec_sound);
 
 function obs_connect() {
@@ -80,19 +87,30 @@ function obs_scene_change(scene_name) {
 }
 
 function obs_menu_scene_change() {
-  obs_scene_change(obs_menu_scene_name);
+  let menu_scene_name = obs_menu_scene_name;
+  if (song_scene_menu_name !== false) menu_scene_name = song_scene_menu_name;
+  obs_scene_change(menu_scene_name);
 }
 
 function obs_game_scene_change() {
-  obs_scene_change(obs_game_scene_name);
+  let game_scene_name = obs_game_scene_name;
+  if (song_scene_game_name !== false) game_scene_name = song_scene_game_name;
+  obs_scene_change(game_scene_name);
 }
 
 function obs_start_scene_change() {
-  if (obs_start_scene_duration > 0) {
-    obs_scene_change(obs_start_scene_name);
-    obs_timeout_id = setTimeout(obs_game_scene_change, obs_start_scene_duration * 1000);
+  let start_scene_duration = obs_start_scene_duration;
+  let start_scene_name = obs_start_scene_name;
+  let game_scene_name = obs_game_scene_name;
+  if (song_scene_start_duration !== false) start_scene_duration = song_scene_start_duration;
+  if (song_scene_start_name !== false) start_scene_name = song_scene_start_name;
+  if (song_scene_game_name !== false) game_scene_name = song_scene_game_name;
+  
+  if (start_scene_duration > 0) {
+    obs_scene_change(start_scene_name);
+    obs_timeout_id = setTimeout(obs_game_scene_change, start_scene_duration * 1000);
   } else {
-    obs_scene_change(obs_game_scene_name);
+    obs_scene_change(game_scene_name);
   }
 }
 
@@ -102,17 +120,33 @@ if (typeof song_scene_json !== "undefined"){
 
 function song_scene_check(data) {
   if (song_scene_list === false) return;
+  song_scene_next_time = false;
+  song_scene_next_name = "";
+  song_scene_time_index = 0;
+  song_scene_time_list = false;
+  song_scene_start_change = true;
+  song_scene_end_change = true;
+  song_scene_game_name = false;
+  song_scene_start_name = false;
+  song_scene_start_duration = false;
+  song_scene_menu_name = false;
+  song_scene_end_name = false;
+  song_scene_end_duration = false;
   for(let i = 0; i < song_scene_list.length; i++){
     if (data.status.beatmap.songHash.toUpperCase() === song_scene_list[i].hash.toUpperCase()){
-      song_scene = song_scene_list[i].timelist;
-      song_scene_start_change = song_scene_list[i].startchange;
-      song_scene_end_change = song_scene_list[i].endchange;
+      if (typeof song_scene_list[i].timelist !== "undefined") song_scene_time_list = song_scene_list[i].timelist;
+      if (typeof song_scene_list[i].startchange !== "undefined") song_scene_start_change = song_scene_list[i].startchange;
+      if (typeof song_scene_list[i].endchange !== "undefined") song_scene_end_change = song_scene_list[i].endchange;
+      if (typeof song_scene_list[i].gamescene !== "undefined") song_scene_game_name = song_scene_list[i].gamescene;
+      if (typeof song_scene_list[i].startscene !== "undefined") song_scene_start_name = song_scene_list[i].startscene;
+      if (typeof song_scene_list[i].startduration !== "undefined") song_scene_start_duration = song_scene_list[i].startduration;
+      if (typeof song_scene_list[i].menuscene !== "undefined") song_scene_menu_name = song_scene_list[i].menuscene;
+      if (typeof song_scene_list[i].endscene !== "undefined") song_scene_end_name = song_scene_list[i].endscene;
+      if (typeof song_scene_list[i].endduration !== "undefined") song_scene_end_duration = song_scene_list[i].endduration;
       song_scene_next();
       return;
     }
   }
-  song_scene_start_change = true;
-  song_scene_end_change = true;
 }
 
 function time_msce_cnv(str) {
@@ -131,7 +165,8 @@ function time_msce_cnv(str) {
 }
 
 function song_scene_next(){
-  let next_scene = song_scene.shift();
+  if (song_scene_time_list === false) return;
+  let next_scene = song_scene_time_list[song_scene_time_index];
   if (typeof next_scene === "undefined"){
     song_scene_next_time = false;
     song_scene_next_name = "";
@@ -139,10 +174,11 @@ function song_scene_next(){
     song_scene_next_time = time_msce_cnv(next_scene.time);
     song_scene_next_name = next_scene.scene;
   }
+  ++song_scene_time_index;
 }
 
 ex_timer_update.push((time, delta, progress, percentage) => {
-  if (song_scene === false) return;
+  if (song_scene_time_list === false) return;
   if (song_scene_next_time === false) return;
   if (delta >= song_scene_next_time){
     obs_scene_change(song_scene_next_name);
@@ -170,27 +206,34 @@ ex_songStart.push((data) => {
 
 function obs_end_scene_change() {
   let obs_end_scene_duration = 0;
-  switch (obs_end_event) {
-    case 'fullcombo':
-      obs_end_scene_duration = obs_fullcombo_scene_duration;
-      if (obs_end_scene_duration > 0) obs_scene_change(obs_fullcombo_scene_name);
-      break;
-    case 'finish':
-      obs_end_scene_duration = obs_finish_scene_duration;
-      if (obs_end_scene_duration > 0) obs_scene_change(obs_finish_scene_name);
-      break;
-    case 'fail':
-      obs_end_scene_duration = obs_fail_scene_duration;
-      if (obs_end_scene_duration > 0) obs_scene_change(obs_fail_scene_name);
-      break;
-    case 'pause':
-      obs_end_scene_duration = obs_pause_scene_duration;
-      if (obs_end_scene_duration > 0) obs_scene_change(obs_pause_scene_name);
+  if (song_scene_end_name !== false && song_scene_end_duration !== false){
+    if (song_scene_end_duration > 0) obs_scene_change(song_scene_end_name);
+    obs_end_scene_duration = song_scene_end_duration;
+  } else {
+    switch (obs_end_event) {
+      case 'fullcombo':
+        obs_end_scene_duration = obs_fullcombo_scene_duration;
+        if (obs_end_scene_duration > 0) obs_scene_change(obs_fullcombo_scene_name);
+        break;
+      case 'finish':
+        obs_end_scene_duration = obs_finish_scene_duration;
+        if (obs_end_scene_duration > 0) obs_scene_change(obs_finish_scene_name);
+        break;
+      case 'fail':
+        obs_end_scene_duration = obs_fail_scene_duration;
+        if (obs_end_scene_duration > 0) obs_scene_change(obs_fail_scene_name);
+        break;
+      case 'pause':
+        obs_end_scene_duration = obs_pause_scene_duration;
+        if (obs_end_scene_duration > 0) obs_scene_change(obs_pause_scene_name);
+    }
   }
+  let menu_scene_name = obs_menu_scene_name;
+  if (song_scene_menu_name !== false) menu_scene_name = song_scene_menu_name;
   if (obs_end_scene_duration > 0) {
     obs_timeout_id = setTimeout(obs_menu_scene_change, obs_end_scene_duration * 1000);
   } else {
-    obs_scene_change(obs_menu_scene_name);
+    obs_scene_change(menu_scene_name);
   }
 }
 
--- js/song-scene.js
+++ js/song-scene.js
@@ -1,23 +1,8 @@
 const song_scene_json = `
 [
   {
-    "hash": "6D1FA74C517A03EA76170345789C85F9077DC7A4",
-    "startchange": false,
-    "endchange": false,
-    "timelist": [
-      {
-         "time": "0:00", 
-         "scene": "TEST1"
-      },
-      {
-         "time": "0:10.550",
-         "scene": "TEST2"
-      }
-    ]
-  },
-  {
-    "hash": "02D18079CE5C7D0179DADD9D77A3D8C729D8C1D8",
-    "startchange": false,
+    "hash": "06121351C6BC732112B20D2C524FB84C036DDF5E",
+    "startchange": true,
     "endchange": true,
     "timelist": [
       {
@@ -27,6 +12,44 @@ const song_scene_json = `
       {
          "time": "0:20",
          "scene": "TEST1"
+      }
+    ]
+  },
+  {
+    "hash": "02D18079CE5C7D0179DADD9D77A3D8C729D8C1D8",
+    "startchange": true,
+    "gamescene": "GAME",
+    "endchange": true,
+    "menuscene": "MENU",
+    "timelist": [
+      {
+         "time": "0:10.500", 
+         "scene": "TEST3"
+      },
+      {
+         "time": "0:20",
+         "scene": "TEST1"
+      }
+    ]
+  },
+  {
+    "hash": "6D1FA74C517A03EA76170345789C85F9077DC7A4",
+    "startchange": true,
+    "gamescene": "GAME",
+    "startscene" : "START",
+    "startduration" : 3,
+    "endchange": true,
+    "menuscene": "MENU",
+    "endscene": "END",
+    "endduration" : 3,
+    "timelist": [
+      {
+         "time": "0:15", 
+         "scene": "TEST2"
+      },
+      {
+         "time": "0:20.550",
+         "scene": "TEST3"
       }
     ]
   }
